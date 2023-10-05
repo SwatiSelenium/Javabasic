5b3a8fc406f00cc321180a3af85038cccb61fe8e
Swati <swati.bhorkhade@gmail.com>
2023-10-05 22:23:28 +0530

    sb

---
+++ Charity.java
@@ -0,0 +1,12 @@
+package mypackage1;
+
+public class Charity 
+{
+	public static void main(String[] args) 
+	{
+		int s=2000;
+		int yearly=s*12;
+		double charity=yearly*0.10;
+		System.out.println(charity);
+	}
+}
