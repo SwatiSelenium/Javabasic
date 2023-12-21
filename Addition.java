a88dde61cfd567434458bb6107eae329485d0799
Swati <swati.bhorkhade@gmail.com>
2023-12-21 10:10:48 +0530

    addition prg

---
+++ Addition.java
@@ -0,0 +1,18 @@
+package mypackage1;
+
+public class Addition 
+{
+	static void add()
+	{
+		System.out.println("addition is");
+		int a=500;
+		int b=300;
+		int c= a+b;
+		System.out.println(c);
+	}
+	
+	public static void main(String[] args) 
+	{
+		add();
+	} 
+}
